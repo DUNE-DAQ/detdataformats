961f241592f3a95e6d05cfa49fcb53cd11e9eab8
Giovanna Lehmann Miotto <glehmann@np04-srv-019.cern.ch>
2022-07-12 10:10:29 +0200

    added detector ID structure

---
+++ include/detdataformats/DetID.hpp
@@ -0,0 +1,95 @@
+/**
+ * @file DetID.hpp Identification for the detector in the raw data
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef DETDATAFORMATS_INCLUDE_DETDATAFORMATS_DETID_HPP_
+#define DETDATAFORMATS_INCLUDE_DETDATAFORMATS_DETID_HPP_
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <iomanip>
+#include <istream>
+#include <limits>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <tuple>
+
+namespace dunedaq::detdataformats {
+
+/**
+ * @brief DetID is a versioned structure containing the 6 bits field of the unique identifier for a subdetector in the raw data.
+ * For convenience this field is expanded to 16 bits.
+ */
+struct DetID
+{
+
+  using Version_t = uint16_t;   // NOLINT(build/unsigned)
+  using Subdetector_t = uinti16_t; // NOLINT(build/unsigned)
+
+  /**
+   * @brief The Subdetector enum describes the kind of source we're dealing with
+   */
+
+  enum class Subdetector : Subdetector_t
+  {
+    kUNDEFINED = 0,
+    kHDPDS = 1,
+    kHDTPC = 2,
+    kVDCPDS = 3,
+    kVDMPDS = 4,
+    kVDBTPC = 5,
+    kVDTTPC = 6,
+    kNDLAR = 32,
+    kNDGAR = 33,
+    kCountOfEnums // Add any new Subdetector enums above this line. This is the count of enums besides itself.
+  };
+
+  /**
+   * @brief The version of this DetID struct.
+   */
+  static constexpr Version_t s_det_id_version = 1;
+
+  /**
+   * @brief Version number of the DetID
+   */
+  Version_t version{ s_det_id_version };
+  /**
+   * @brief The general subdetector of the source of the data
+   */
+  Subdetector subdetector{ Subdetector::kUNDEFINED };
+
+  /**
+   * @brief Unique identifier of the source of the data
+   */
+  ID_t id{ s_invalid_id };
+
+  DetID() = default;
+
+  DetID(const Subdetector& subdetector_arg)
+    : subdetector(subdetector_arg)
+  {}
+
+  std::string to_string() const
+  {
+    std::ostringstream ostr;
+    ostr << subdetector_to_string(subdetector);
+    return ostr.str();
+  }
+
+  bool is_in_valid_state() const noexcept { return subdetector != Subdetector::kUNDEFINED; }
+
+  inline static std::string subdetector_to_string(const Subdetector& type);
+  inline static Subdetector string_to_subdetector(const std::string& typestring);
+};
+
+} // namespace dunedaq::detdataformats
+
+#include "detail/DetID.hxx"
+
+#endif // DETDATAFORMATS_INCLUDE_DETDATAFORMATS_DETID_HPP_
---
+++ include/detdataformats/detail/DetID.hxx
@@ -0,0 +1,114 @@
+
+namespace dunedaq::detdataformats {
+
+static_assert(DetID::s_det_id_version == 1,
+              "This is intentionally designed to tell the developer to update the static_assert checks (including this "
+              "one) when the version is bumped");
+static_assert(sizeof(DetID) == 4, "DetID struct size different than expected!");
+static_assert(offsetof(DetID, version) == 0, "DetID version field not at expected offset");
+static_assert(offsetof(DetID, subdetector) == 2, "DetID subdetector field not at expected offset");
+
+/**
+ * @brief Stream a Subdetector instance in a human-readable form
+ * @param o Stream to output to
+ * @param id Subdetector to stream
+ * @return Stream instance for further streaming
+ */
+inline std::ostream&
+operator<<(std::ostream& o, DetID::Subdetector const& type)
+{
+  return o << DetID::subdetector_to_string(type);
+}
+
+/**
+ * @brief Stream a DetID instance in a human-readable form
+ * @param o Stream to output to
+ * @param id DetID to stream
+ * @return Stream instance for further streaming
+ */
+inline std::ostream&
+operator<<(std::ostream& o, DetID const& det_id)
+{
+  return o << "subdetector: " << det_id.subdetector ;
+}
+
+/**
+ * @brief Read a DetID::Subdetector from a string stream
+ * @param is Stream to read from
+ * @param id Subdetector to fill
+ * @return Stream instance for further streaming
+ */
+inline std::istream&
+operator>>(std::istream& is, DetID::Subdetector& t)
+{
+  std::string tmp;
+  is >> tmp;
+
+  t = DetID::string_to_subdetector(tmp);
+
+  return is;
+}
+/**
+ * @brief Read a DetID from a string stream
+ * @param is Stream to read from
+ * @param id DetID to fill
+ * @return Stream instance for further streaming
+ */
+inline std::istream&
+operator>>(std::istream& is, DetID& det_id)
+{
+  std::string tmp;
+  is >> tmp >> det_id.subdetector; 
+
+  return is;
+}
+
+std::string
+DetID::subdetector_to_string(const Subdetector& type)
+{
+  switch (type) {
+    case Subdetector::kHDPDS:
+      return "HDPDS";
+    case Subdetector::kHDTPC:
+      return "HDTPC";
+    case Subdetector::kVDCPDS:
+      return "VDCPDS";
+    case Subdetector::kVDMPDS:
+      return "VDMPDS";
+    case Subdetector::kVDBTPC:
+      return "VDBTPC";
+    case Subdetector::kVDTTPC:
+      return "VDTTPC";
+    case Subdetector::kNDLAR:
+      return "NDLAR";
+    case Subdetector::kNDGAR:
+      return "NDGAR";
+    default:
+      return "UNDEFINED";
+  }
+}
+
+DetID::Subdetector
+DetID::string_to_subdetector(const std::string& typestring)
+{
+  if (typestring == "HDPDS")
+    return Subdetector::kHDPDS;
+  if (typestring == "HDTPC")
+    return Subdetector::kHDTPC;
+  if (typestring == "VDCPDS")
+    return Subdetector::kVDCPDS;
+  if (typestring == "VDMPDS")
+    return Subdetector::kVDMPDS;
+  if (typestring == "VDBTPC")
+    return Subdetector::kVDBTPC;
+  if (typestring == "VDTTPC")
+    return Subdetector::kVDTTPC;
+  if (typestring == "NDLAR")
+    return Subdetector::kNDLAR;
+  if (typestring == "NDGAR")
+    return Subdetector::kNDGAR;
+
+  return Subdetector::kUNDEFINED;
+}
+
+} // namespace dunedaq::detdataformats
