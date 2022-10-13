349ecec718f648697512334f77fcecff30c5a3ed
Julia Tena Vidal <jtenavid@fnal.gov>
2022-10-13 08:01:05 -0500

    adding more than one header

--- include/detdataformats/mpd/MPDFrame.hpp
+++ include/detdataformats/mpd/MPDFrame.hpp
@@ -32,22 +32,36 @@ class MPDFrame
   typedef uint32_t word_t ; // NOLINT(build/unsigned)
   static constexpr unsigned int num_samples = 2048 ;
   
-  struct MPDHeader {
+  struct MPDOSHeader {
     word_t timestamp_sync ; // 0x3f60b8a8 ;
     word_t timestamp_length ; // 0x00000008 ;
     uint64_t timestamp_OS ; // timestamp taken from the operating system
+  };
+
+  struct MPDEventHeader {  
     word_t SyncMagic ; // 0x2A502A50 ;
     word_t length ;
     word_t event_num ; 
-    //MpdDeviceHeader
+  };
+
+  struct MPDDeviceHeader {
     word_t device_serial_num ;
     word_t device_length : 24, device_model_id : 8;  
+  };
+
+  struct MPDTriggerHeader {
     //MStreamTriggerHeader
     word_t trigger_type: 2, trigger_length: 22, trigger_channel_number: 8 ; 
+  };
+
+  struct MPDTriggerDataHeader {
     //Trigger Data
     word_t event_timestamp_1: 32 ; 
     word_t flags: 2, event_timestamp_2: 30 ; 
     uint64_t channel_bit_mask ; 
+  };
+
+  struct MPDDataHeader {
     //Data header
     word_t data_type: 2, data_length: 22, channel_number: 8 ;
   };
@@ -56,7 +70,13 @@ class MPDFrame
   // =======================================================
   // Data members 
   // =======================================================
-  MPDHeader header ; 
+  MPDOSHeader OSheader ; 
+  MPDEventHeader event_header ; 
+  MPDDeviceHeader device_header ; 
+  MPDTriggerHeader trigger_header ; 
+  MPDTriggerDataHeader trigger_data_header ; 
+  MPDDataHeader data_header ; 
+
   //MStream Data
   uint64_t _null; // unused words
   uint16_t data[num_samples];
@@ -76,7 +96,7 @@ class MPDFrame
    */
   uint64_t get_timestamp() const {
     // in nanoseconds
-    return (uint64_t)header.event_timestamp_1 | ((uint64_t)header.event_timestamp_2 << 32 ) ; 
+    return (uint64_t)trigger_data_header.event_timestamp_1 | ((uint64_t)trigger_data_header.event_timestamp_2 << 32 ) ; 
   }
 
 };
