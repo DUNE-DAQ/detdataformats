3cb3b2b29371ad1f0be8a4ff5cfb8663333dcd5c
Naseem Khan <nkhan@dunegpvm01.fnal.gov>
2023-01-19 12:18:43 -0600

    TOAD format for v3.2.0

---
+++ include/detdataformats/toad/TOADFrameOverlay.hpp
@@ -0,0 +1,36 @@
+#ifndef DETDATAFORMATS_INCLUDE_DATAFORMATS_TOADFRAMEOVERLAY_HPP_
+#define DETDATAFORMATS_INCLUDE_DATAFORMATS_TOADFRAMEOVERLAY_HPP_
+
+namespace dunedaq {
+namespace detdataformats {
+namespace toad {
+
+struct TOADFrameOverlay {
+  unsigned fec: 10;
+  unsigned channel: 5;
+  unsigned tstmp: 32;
+  bool hdr_par_check: 1;
+  bool data_par_check: 1;
+  size_t n_samples;
+  size_t n_bytes;
+  unsigned short toadsamples[];
+
+  uint64_t get_timestamp() const {
+    return (uint64_t)tstmp;
+  }
+  
+  void set_timestamp(uint32_t new_tstmp){
+    tstmp = new_tstmp;
+  }
+ 
+  std::size_t get_size() {
+    return n_bytes;
+    //return (3*sizeof(unsigned) + 2*sizeof(bool) + sizeof(size_t)+n_samples*sizeof(unsigned short));
+  }
+};
+ 
+} // namespace toad
+} // namespace detdataformats
+} // namespace dunedaq
+#endif                           
+
---
+++ include/detdataformats/toad/TOADObjectOverlay.hpp
@@ -0,0 +1,39 @@
+#ifndef DETDATAFORMATS_INCLUDE_DATAFORMATS_TOADOBJECTOVERLAY_HPP_
+#define DETDATAFORMATS_INCLUDE_DATAFORMATS_TOADOBJECTOVERLAY_HPP_
+
+#include "TOADFrame.hpp"
+#include "TOADFrameOverlay.hpp"
+
+namespace dunedaq {
+namespace detdataformats {
+namespace toad {
+
+class TOADObjectOverlay
+{
+  public:
+  TOADFrameOverlay* overlay;
+  void write_toad_overlay(TOADFrame& object, void* buffer, size_t nbytes){
+    overlay = reinterpret_cast<TOADFrameOverlay*>(buffer);
+    overlay->fec = object.fec;
+    overlay->channel = object.channel;
+    overlay->tstmp = object.tstmp;
+    overlay->hdr_par_check = object.hdr_par_check;
+    overlay->data_par_check = object.data_par_check;
+    overlay->n_samples = object.toadsamples.size();
+    overlay->n_bytes = nbytes;
+    size_t sample_num = object.toadsamples.size();
+    for (size_t i = 0; i < sample_num; i++) {
+      overlay->toadsamples[i] = object.toadsamples[i];
+    }
+    
+  }
+  size_t get_toad_overlay_nbytes(TOADFrame& object) {
+    return sizeof(TOADFrameOverlay) + object.toadsamples.size() * sizeof(unsigned short);
+  }
+    
+};
+
+} // namespace toad
+} // namespace detdataformats
+} // namespace dunedaq
+#endif
