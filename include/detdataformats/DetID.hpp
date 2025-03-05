0238d47c984382dac74fa2729738015ab016c1a8
Wesley Robert Ketchum <wketchum@fnal.gov>
2025-03-05 18:22:36 +0100

    add a VD_CRT detid for np02 crt systems

--- include/detdataformats/DetID.hpp
+++ include/detdataformats/DetID.hpp
@@ -47,6 +47,7 @@ struct DetID
     kVD_MembranePDS = 9,
     kVD_BottomTPC = 10,
     kVD_TopTPC = 11,
+    kVD_CRT = 12,
     kNDLAr_TPC = 32,
     kNDLAr_PDS = 33,
     kND_GAr = 34
--- include/detdataformats/detail/DetID.hxx
+++ include/detdataformats/detail/DetID.hxx
@@ -83,6 +83,8 @@ DetID::subdetector_to_string(const Subdetector& type)
       return "VD_Bottom_TPC";
     case Subdetector::kVD_TopTPC:
       return "VD_Top_TPC";
+    case Subdetector::kVD_CRT:
+      return "VD_CRT";
     case Subdetector::kNDLAr_TPC:
       return "NDLAr_TPC";
     case Subdetector::kNDLAr_PDS:
@@ -113,6 +115,8 @@ DetID::string_to_subdetector(const std::string& typestring)
     return Subdetector::kVD_BottomTPC;
   if (typestring == "VD_Top_TPC")
     return Subdetector::kVD_TopTPC;
+  if (typestring == "VD_CRT")
+    return Subdetector::kVD_CRT;
   if (typestring == "NDLAr_TPC")
     return Subdetector::kNDLAr_TPC;
   if (typestring == "NDLAr_PDS")
